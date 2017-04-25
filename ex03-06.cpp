4e7741b723786c1c2aae8b7d9edfe91d763800e9
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-04-25 17:46:51 -0300

    Novo exercicio e caso de teste

--- ex03-06.cpp
+++ ex03-06.cpp
@@ -5,9 +5,33 @@
 #include <queue>
 #include <math.h>
 
+#define PRIMO 131071
+
 using namespace std;
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[]) {
 
-  return 0;
+    char num;
+    long total = 0;
+
+    while(cin >> num) {
+        //se achou o final do arquivo, para de analisar
+        if(num == EOF)
+            break;
+        //Se encontrou o mostrador de final de numero, verifica o total
+        if(num == '#') {
+          if(total == 0)
+            cout << "YES" << endl; // caso tenha dado divisao exata
+          else
+            cout << "NO" << endl; // caso a divisao nao tenha sido exata
+
+          //zera o total quando troca de linha
+          total = 0;
+        } else{
+            //enquanto nao for # continua rodando e fazendo as contas
+            total = total*2 + num - '0';
+            total = total%PRIMO;
+        }
+    }
+    return 0;
 }
