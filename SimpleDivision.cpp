469f0cc9963abe0c4d178762193917eb292d8e9f
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-04-26 17:27:40 -0300

    ëx quase completo, faltando arrumar algumas contas

--- SimpleDivision.cpp
+++ SimpleDivision.cpp
@@ -17,6 +17,9 @@ int main(int argc, char const *argv[]){
     //Criando um vetor novo para nao atrapalhar outras linhas
     int vet[LIMITE];
     int i = 0;
+    int total = 0;
+    int aux = 0;
+    int temp = 0;
     //Se encontrou o zero aqui, acabaram as entradas
     if(num == 0){
       //Sai do while
@@ -36,9 +39,43 @@ int main(int argc, char const *argv[]){
           i++;
         }
       }
+      //Comecar fazer a conta analisando o vetor com os inteiros armazenados
+      for(int j = 1; j < i; j++){
+        //Caso especial para a posicao 1 e 0 do vetor
+        if(j == 1){
+          //subtraindo os valores
+          total = vet[1] - vet[0];
+          //Pode dar negativo entao pegar o modulo
+          total = abs(total);
+        }else{//caso nao sejam os primeiros valores
+            //subtraindo os outros valores armazenados no vetor
+            aux = vet[i] - vet[i-1];
+            //pegando o modulo
+            aux = abs(aux);
 
+            //Atualizando o total, usando total e aux
+            if(total == 0){
+              total = aux;
+            }else if(aux == 0){
+              //total continua com o mesmo valor
+            }
+            //realizando as contas com mod
+            int aux_total = total;
+            int aux_aux = aux;
+            //Calcula o maior divisor
+            while(aux_total%aux_aux != 0){
+              //atualiza os valores
+              temp = aux_total;
+              aux_total = aux_aux;
+              aux_aux = temp%aux_aux;
+            }
+
+            //atualizando o valor do total
+            total = aux_aux;
+        }
+      }//fim do for com as contas
+      cout << total << endl;
     }
-
 
   }
 
--- teste.cpp
+++ teste.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 int gcd(int x, int y) {
-    if(!x)  return y;
-    if(!y)  return x;
+    if(x == 0)  return y;
+    if(y == 0)  return x;
     int t;
     while(x%y) {
         t = x, x = y, y = t%y;
     }
     return y;
 }
+
 int main() {
     int a[1000];
     while(scanf("%d", a) == 1) {
