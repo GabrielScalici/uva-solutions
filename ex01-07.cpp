bcb0dda3d5a4b661cd4e4fe817bc3ed2916668a6
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-05-02 10:12:01 -0300

    Inicio da logica do exercicio

--- ex01-07.cpp
+++ ex01-07.cpp
@@ -10,5 +10,20 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 
+  int campsites;
+  int nights;
+
+  cin >> campsites;
+  cin >> nights;
+
+  int cont = campsites + 1;
+
+  while(cont){
+
+    cont--;
+  }
+
+
+
   return 0;
 }
