3eec699b0a50c62c231d35ef47b207a5684eecdd
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-05-03 17:21:09 -0300

    Ïnicio do exE do contest em aula

---
+++ exE.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <stdio.h>
+#include <cstring>
+
+//tamanho maximo do numero analisado
+#define MAX 2000000
+
+using namespace std;
+
+int verifica(){
+
+  return 1;
+}
+
+
+int main(int argc, char const *argv[]){
+
+   int num;
+
+   //analisando caso por caso até o zero
+   while(cin >> num){
+     //parando quando achar o zero
+     if(num == 0){
+       break;
+     }
+     //anotando o valor em uma variavel reserva
+     int aux = num;
+     int inicio_contagem = 2;
+     //iniciando fila de respostas
+     queue <int> fila;
+
+     //Verificando os fatoriais do numero
+
+   }
+
+
+
+
+  return 0;
+}
