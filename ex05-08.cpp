a8ca7c3f31566710934441ba723e23ac6d19d941
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-05-14 16:32:07 -0300

    ëx 5 da aula 8 pronto para fazer

---
+++ ex05-08.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+
+  return 0;
+}
