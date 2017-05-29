daa89f4f70e91d85bd2c283eee57a0aa1362b1fd
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-05-29 18:41:40 -0300

    ex atualizado

--- ex01-10.cpp
+++ ex01-10.cpp
@@ -8,120 +8,73 @@
 #include <queue>
 #include <math.h>
 
-#define N 10000
-#define COMP 1e9
-#define VERDADEIRO 1
-#define UN unsigned
+
+#define AUX 1e9
 
 using namespace std;
 
-
-struct Node{
-    UN int v, w;
-    Node(){}
-    Node(UN int vv, UN int ww) :v(vv), w(ww){}
-    bool operator<(const Node& a)const{return w > a.w; }
-};
-
-vector<Node>vetor[N];
-
-//algoritmo dijkstra
-int dij(UN int s, UN int t, UN int n)
-{
-    UN int visitado[N]= {};
-    UN int dist[N];
-    UN int proximo;
-
-    // usando func fill
-    fill(dist, dist + n, COMP);
-
-    dist[s] = 0;
-    priority_queue<Node> fila;
-    fila.push(Node(s, 0));
-
-    //roda ate achar alguma saida pelo algoritmo
-    while (VERDADEIRO){
-        proximo = -1;
-        //arrumando a fila
-        while (fila.empty() == 0 && visitado[proximo = fila.top().v]){
-            fila.pop();
-        }
-        if (proximo == -1 || proximo == t){
-          //ja retorna e sai da func
-          return dist[t];
-        }
-
-        //marca como visitado
-        visitado[proximo] = 1;
-        for (Node node : vetor[proximo]){
-            if (visitado[node.v] == 0 && (dist[proximo] + node.w) < dist[node.v]){
-                dist[node.v] = (dist[proximo] + node.w);
-                //adiciona na fila
-                fila.push(Node(node.v, dist[node.v]));
-            }
-        }
-    }
-    //retorna o final de toda a analise
-    return dist[t];
-}
+vector<int> lista;
 
 //imprimindo a resposta
-void imprimir(UN int c, UN int res){
-  //Imprimindo para o usuario
-  cout << "Case #" << (c+1) << ": ";
-
-  //verificando se teve resposta ou nao
-  if (res != COMP)
-      cout << res << endl;
-  else
-      cout << "unreachable" << endl;
-}
-
-void atualiza(UN int tam){
-  UN int u, v, w;
-  for (UN int i = 0; i < tam; i++){
-    cin >> u;
-    cin >> v;
-    cin >> w;
-    vetor[u].push_back(Node(v, w));
-    vetor[v].push_back(Node(u, w));
-  }
-}
-
-void limpar_fila(UN int tam){
-  for (UN int i = 0; i < tam; i++){
-    vetor[i].clear();
+void imprime(unsigned int i, unsigned int T){
+  cout << "Case #" << i+1 << ": ";
+  if (lista[T] != AUX){
+      cout << lista[T] << endl;
+  }else{
+    cout << "unreachable" << endl;
   }
 }
 
 int main(int argc, char const *argv[]){
-    UN int quantidade;
-    UN int res;
 
-    //pegando as primeiras informacoes
-    cin >> quantidade;
-    for (UN int c = 0; c < quantidade; c++){
-        //variaveis unsigned usadas na logica
-        UN int n, m, s, t;
+  int N, n, m, S, T, a, b, w;
 
-        cin >> n;
+  vector<vector<pair<int, int> > > Result;
+  priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > fila_p;
 
-        //limpando fila
-        limpar_fila(n);
+  cin >> N;
 
-        cin >> m;
-        cin >> s;
-        cin >> t;
+   for(int i = 0; i < N; i++){
 
-        //percorrendo atualizando a fila
-        atualiza(m);
+       cin >> n;
+       cin >> m;
+       cin >> S;
+       cin >> T;
+       Result.assign(n, vector<pair<int, int> >());
 
-        //fazendo o lg dijstra
-        res = dij(s, t, n);
+       while (m > 0) {
 
-        //imprimindo os resultados
-        imprimir(c, res);
+           cin >> a;
+           cin >> b;
+           cin >> w;
+           Result[a].push_back(pair<int, int>(b, w));
+           Result[b].push_back(pair<int, int>(a, w));
+
+           m--;
+       }
+
+       lista.assign(n, AUX);
+       lista[S] = 0;
+       fila_p.push(pair<int, int>(0, S));
+
+       while (fila_p.empty() == 0) {
+           pair<int, int> front = fila_p.top(); fila_p.pop();
+           int d = front.first, u = front.second;
+           if (d == lista[u]) {
+
+           int tam = Result[u].size();
+           for(int j = 0; j < tam ; j++){
+               pair<int, int> v = Result[u][j];
+               if (lista[u] + v.second < lista[v.first]) {
+                   lista[v.first] = lista[u] + v.second;
+                   fila_p.push(pair<int, int>(lista[v.first], v.first));
+               }
+           }
+       }
 
     }
-    return 0;
+
+    imprime(i, T);
+
+   }
 }
